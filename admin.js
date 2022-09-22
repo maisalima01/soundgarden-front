04a1fe0e8cdb583613be9c2e042a3db1d9b745b9
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2022-09-22 14:35:20 -0300

    adicionado reservas e corrigido admin

---
+++ admin.js
@@ -0,0 +1,37 @@
+const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
+
+[1, 2, 3, 4, 5]
+"1, 2, 3, 4, 5" 
+async function getEvents(){
+  try {
+    const response = await fetch(`${BASE_URL}/events`)
+    const events = await response.json();
+
+    const tbody = document.getElementsByTagName('tbody')[0];
+
+    // preencher o HTML com os dados dos eventos.
+    events.forEach((event, index) => {
+      const row = `
+        <tr>
+          <th scope="row">${index+1}</th>
+          <td>${new Date(event.scheduled).toLocaleString('pt-BR')}</td>
+          <td>${event.name}</td>
+          <td>${event.attractions.join(', ')}</td>
+          <td>
+            <a href="reservas.html?id=${event._id}" class="btn btn-dark">ver reservas</a>
+            <a href="editar.html?id=${event._id}" class="btn btn-secondary">editar</a>
+            <a href="excluir-evento.html?id=${event._id}" class="btn btn-danger">excluir</a>
+          </td>
+        </tr>
+      `
+
+      tbody.insertRow().innerHTML = row;
+    })
+  } catch (error) {
+    console.log(error);
+    alert("Ocorreu um erro inesperado. Tente novamente, por favor.");
+  }
+}
+
+getEvents();
+
---
+++ reservas.js
@@ -0,0 +1,39 @@
+const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+
+async function getBookings(id) {
+  try {
+    const response = await fetch(`${BASE_URL}/bookings/event/${id}`);
+    const bookings = await response.json();
+
+    const table = document.getElementsByTagName("tbody")[0];
+
+    bookings.forEach((booking, index) => {
+      const row = `
+        <tr>
+          <th scope="row">${index + 1}</th>
+          <td>${booking.owner_name}</td>
+          <td>${booking.owner_email}</td>
+        </tr>
+      `;
+
+      table.insertRow().innerHTML = row;
+    });
+  } catch (error) {
+    console.log(error);
+    alert("Ocorreu um erro inesperado. Tente novamente, por favor.");
+  }
+}
+
+async function getEvent(id) {
+  const response = await fetch(`${BASE_URL}/events/${id}`);
+  const event = await response.json();
+
+  document.getElementById("eventName").innerHTML = `Reservas do ${event.name}`;
+}
+
+getEvent(params.id);
+getBookings(params.id);
+
